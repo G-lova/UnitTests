b239f3e98455f7b09007d814da76a6cd89e8b2b3
Lyubov Gavrilova <suslik.lyuba@gmail.com>
2023-10-13 19:29:38 +0300

    NumbersTest added

---
+++ Test/seminar3/hw/MainHWTest.java
@@ -0,0 +1,85 @@
+import org.junit.Test;
+import seminar3.hw.MainHW;
+
+import static org.junit.Assert.*;
+
+public class MainHWTest {
+    private MainHW number;
+    @before
+    public void createNumber {
+        number = new MainHW();
+    }
+
+    @Test
+    public void testEvenNumber() {
+        assertTrue(number.evenOddNumber(0));
+        assertTrue(number.evenOddNumber(2));
+        assertTrue(number.evenOddNumber(4));
+        assertTrue(number.evenOddNumber(100));
+    }
+
+    @Test
+    public void testOddNumber() {
+        assertFalse(number.evenOddNumber(1));
+        assertFalse(number.evenOddNumber(3));
+        assertFalse(number.evenOddNumber(5));
+        assertFalse(number.evenOddNumber(101));
+    }
+
+    @Test
+    public void testNegativeEvenNumber() {
+        assertTrue(number.evenOddNumber(-2));
+        assertTrue(number.evenOddNumber(-4));
+        assertTrue(number.evenOddNumber(-100));
+    }
+
+    @Test
+    public void testNegativeOddNumber() {
+        assertFalse(number.evenOddNumber(-1));
+        assertFalse(number.evenOddNumber(-3));
+        assertFalse(number.evenOddNumber(-101));
+    }
+
+    @Test
+    public void testPositiveEvenNumber() {
+        assertTrue(number.evenOddNumber(2));
+        assertTrue(number.evenOddNumber(4));
+        assertTrue(number.evenOddNumber(100));
+    }
+
+    @Test
+    public void testPositiveOddNumber() {
+        assertFalse(number.evenOddNumber(1));
+        assertFalse(number.evenOddNumber(3));
+        assertFalse(number.evenOddNumber(101));
+    }
+
+
+    @Test
+    public void testNumberInRange() {
+        assertTrue(number.numberInInterval(26));
+        assertTrue(number.numberInInterval(50));
+        assertTrue(number.numberInInterval(99));
+    }
+
+    @Test
+    public void testNumberNotInRange() {
+        assertFalse(number.numberInInterval(25));
+        assertFalse(number.numberInInterval(100));
+        assertFalse(number.numberInInterval(0));
+        assertFalse(number.numberInInterval(101));
+    }
+
+    @Test
+    public void testNegativeNumber() {
+        assertFalse(number.numberInInterval(-26));
+        assertFalse(number.numberInInterval(-50));
+        assertFalse(number.numberInInterval(-99));
+    }
+
+    @Test
+    public void testNumberOnBoundary() {
+        assertFalse(number.numberInInterval(24));
+        assertFalse(number.numberInInterval(101));
+    }
+}
